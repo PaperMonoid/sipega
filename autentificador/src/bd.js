07dbdf76cf34cad854c406d0a84d2065c1238d67
tritiummonoid <daniel.aguila16@tectijuana.edu.mx>
2018-05-07 00:42:18 -0700

    romper autentificador rest api

--- autentificador/src/bd.js
+++ autentificador/src/bd.js
@@ -8,3 +8,26 @@ exports.conectar = () => mysql.createConnection({
 })
 
 exports.terminar = conexion => Promise.resolve(conexion => conexion.end())
+
+exports.analizar = datos => json => conexion =>
+      conexion.query(
+          datos["sentencia"],
+          datos["parametros"].map(llave => json[llave])
+      )
+      .then(resultado => {
+          if (Array.isArray(resultado)) {
+              return resultado.map(elemento => {
+                  const respuesta = {}
+                  for (let llave in datos["resultado"]) {
+                      respuesta[llave] = elemento[datos["resultado"][llave]];
+                  }
+                  return respuesta;
+              });
+          } else {
+              const respuesta = {}
+              for (let llave in datos["resultado"]) {
+                  respuesta[llave] = resultado[datos["resultado"][llave]];
+              }
+              return [respuesta];
+          }
+      });
--- autentificador/src/usuario.js
+++ autentificador/src/usuario.js
@@ -1,35 +1,12 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
+const usuario = require('./usuario.json');
+const { analizar } = require("./bd.js");
 
-exports.consultar = json => conexion => {
-    const { usuarioId } = json
-    return conexion.query(
-        `SELECT * ` +
-            `FROM Usuario ` +
-            `WHERE UsuarioId=${conexion.escape(usuarioId)}`
-    )
-}
+const cifrar = texto => crypto.createHmac("sha256", texto).digest("hex");
 
-exports.crear = json => conexion => {
-    const { clave } = json
-    const hash = crypto.createHmac('sha256', clave).digest('hex')
-    return conexion.query(
-        `INSERT INTO Usuario(UsuarioClave, UsuarioFechaCreacion) ` +
-            `VALUES(${conexion.escape(hash)}, NOW())`
-    ).then(resultado => {
-        return resultado.insertId.toString()
-    })
-}
-
-exports.borrar = json => conexion => {
-    const { usuarioId } = json
-    return conexion.query(
-        `DELETE FROM Usuario ` +
-            `WHERE UsuarioId=${conexion.escape(usuarioId)}`
-    ).then(resultado => {
-        if (resultado.affectedRows > 0) {
-            return usuarioId.toString()
-        } else {
-            throw Error('No se pudo eliminar')
-        }
-    })
-}
+exports.consultar = analizar(usuario["consultar"]);
+exports.borrar = analizar(usuario["borrar"]);
+exports.crear = json => {
+    json["usuarioClave"] = cifrar(json["usuarioClave"]);
+    return analizar(usuario["crear"])(json);
+};
